80709c638c4263c27e71c65bc6861c997e87a5fa
Maciej Wakula <adderek.pl@gmail.com>
2024-10-12 23:48:57 +0200

    Bugfixes

--- README.md
+++ README.md
@@ -9,12 +9,61 @@ mini time plot
 - small size
 - simplicity over features
 - value over time bar chart
+- no crazy scenarios (ex. same X for multiple points)
 
 ## Description
 
 Plots date on X axis, values on Y axis
 Shows current value on mouse over
 Date is always in the international format ISO-8601
+
+## How to use it
+
+Import just like any other module, ex.
+
+```
+<script type="module" src="./mtplot.js"></script>
+```
+
+Initialize with an object, ex.
+
+```
+<div id="my container ID which should be unique across the whole page">
+<div id="second container for another plot">
+...
+<script type="module">
+  import MTPlot from './mtplot.js';
+  document.addEventListener('DOMContentLoaded', () => {
+    // Run after DOM is fully loaded to make sure that our HTML container is there
+
+    const containerID = "my container ID which should be unique across the whole page";
+    const firstPlot = new MTPlot(containerID);
+
+    const secondContainerID = "second container for another plot";
+    const secondPlot = new MTPlot(containerID, [
+      {x:new Date("2020-01-01"), y:0},
+      {x:new Date("2020-01-02"), y:2},
+      {x:new Date("2020-01-05"), y:4},
+      {x:new Date("2020-12-31"), y:3},
+    ]);
+
+    firstPlot.dataProxy.push({x:new Date("2020-01-01"), y:0}); // fast
+    firstPlot.dataProxy[10] = ({x:new Date("2020-01-05"), y:5}) // fast
+    firstPlot.dataProxy.unshift({x:new Date("2020-01-02"), y:2}); // slow as it must shift every value and insert one at the beginning
+
+    // Time between dataProxy first change till plot redraw
+    // default 100ms
+    firstPlot.refreshDelay = 2000; // gives us 2000ms to update plot data (possibly multiple times) before it is refreshed
+  });
+</script>
+```
+
+
+
+/*
+mtplot = new MTPlot("mtplot", [{ x: new Date(2020, 0, 1), y: 1 }, { x: new Date(2020, 0, 2), y: 2 }]);
+*/
+
 
 ## Scripts
 
--- mtplot.js
+++ mtplot.js
@@ -1,117 +1,166 @@
-/* JS module that creates SVG element and appends it to given DOM element (or reuses existing one)
-It is first initialized with new MTPlot(id,data) which has DOM element id that it should be appended to and optional data array.
-Returned value is a new instance of MTPlot class.
-
-The data is an array of {x:Date, y:number} values. It can be accessed by mtplot.data.
-The dataset can be updated. A proxy pattern is used to detect data changes.
-
-x is a JavaScript Date, y is a number
-Whenever data are updated, SVG is updated and redrawn.
-X axis starts at earliest x value and ends at latest x value
-Y axis starts at 0 and ends at max y value
-*/
+// @ts-check
 
 const ns = "http://www.w3.org/2000/svg";
-const w = 365;
 export default class MTPlot {
-    constructor(id, data) {
-        this.id = id;
-        this.data = data;
-        this.proxy = new Proxy(this.data, {
+    /**
+     * Sorted array of {x:Date, y:number} (it is sorted with intervals after update)
+     * @type {Array<{x:Date, y:number}>}
+     * @readonly
+     * @deprecated Use proxy instead (the .data gets sorted internally with a potential delay and unsorted data cause issues)
+    */
+    rawData;
+
+    /** @readonly @type string */
+    id;
+
+    /** @readonly @type SVGSVGElement */
+    svg;
+
+    /** @type boolean */
+    #dirty;
+
+    /** @type number */
+    refreshDelay = 100;
+
+    /**
+     * Proxy (it detects whenever you update it) to a data array
+     * @type {Array<{x:Date, y:number}>}
+     */
+    dataProxy;
+
+    /** @type number|null */
+    #timeout;
+
+    /**
+     * Plot resolution in X axis
+     * Default is initial array length or 365
+     * @type number
+     */
+    w;
+
+    /**
+     * Plot resolution in Y axis
+     * Default is 100
+     * @type number
+     */
+    h = 100;
+
+    /**
+     * Create new MTPlot instance
+     * within parent HTMLElement having given id
+     * and with data from given array of {x:Date, y:number}
+     * @param {string} id Parent element id
+     * @param {Array<{x:Date, y:number}>} [data] Optional initial data array
+     */
+    constructor(id, data=[]) {
+        this.rawData = [...data].sort((a, b) => a.x - b.x);
+        const [w,h] = [this.w = data.length || 365, this.h];
+        this.dataProxy = new Proxy(this.rawData, {
             set: (target, property, value) => {
+                console.log(this.#dirty, this.#timeout, property);
+                if (!this.#dirty) {
+                    this.#dirty = true;
+                    this.#triggerUpdate();
+                }
                 target[property] = value;
-                this.update();
                 return true;
             }
         });
-        this.init();
-    }
-    init() {
-        this.parent = document.getElementById(this.id);
-        if (!this.parent) {
-            this.parent = document.createElement("div");
-            this.parent.setAttribute("id", this.id);
-            document.body.appendChild(this.parent);
+
+        const parent = document.getElementById(id);
+        if (!parent) {
+            throw new Error(`No element #${id}`);
         }
-        this.svg = document.getElementById(this.id).querySelector("svg");
+        this.svg = parent.querySelector("svg");
         if (!this.svg) {
             this.svg = document.createElementNS(ns, "svg");
-            this.svg.setAttribute("version", "1.1");
-            this.svg.setAttribute("width", "100%");
-            this.svg.setAttribute("height", "100%");
-            this.svg.setAttribute("xmlns", ns);
-            this.svg.setAttribute("viewBox", `0 0 ${w} 100`);
-            this.svg.setAttribute("preserveAspectRatio", "none");
-            this.svg.setAttribute("style", "width: 100%; height: 100%;");
-            document.getElementById(this.id).appendChild(this.svg);
+            const attributes = [
+            ["version", "1.1"],
+            ["width", "100%"],
+            ["height", "100%"],
+            ["xmlns", ns],
+            ["viewBox", `0 0 ${w} ${h}`],
+            ["preserveAspectRatio", "none"],
+            ["style", "width: 100%; height: 100%;"]
+            ];
+
+            attributes.forEach(([name, value]) => {
+            this.svg.setAttribute(name, value);
+            });
+            parent.appendChild(this.svg);
         }
+        this.#dirty = true;
         this.update();
     }
-    update() {
-        this.svg.innerHTML = "";
-        this.draw();
-    }
-    draw() {
-        this.drawXAxis();
-        this.drawData();
-    }
-    drawXAxis() {
-        const xAxis = document.createElementNS(ns, "line");
-        xAxis.setAttribute("x1", "0");
-        xAxis.setAttribute("y1", "100");
-        xAxis.setAttribute("x2", w);
-        xAxis.setAttribute("y2", "100");
-        xAxis.setAttribute("stroke", "black");
-        xAxis.setAttribute("stroke-width", "1");
-        this.svg.appendChild(xAxis);
-    }
-    drawYAxis() {
-        const yAxis = document.createElementNS(ns, "line");
-        yAxis.setAttribute("x1", "0");
-        yAxis.setAttribute("y1", "0");
-        yAxis.setAttribute("x2", "0");
-        yAxis.setAttribute("y2", "100");
-        yAxis.setAttribute("stroke", "black");
-        yAxis.setAttribute("stroke-width", "1");
-        this.svg.appendChild(yAxis);
-    }
-    drawData() {
-        if (this.data.length === 0) {
-            return;
-        }
-        const data = this.data;
-        data.sort((a, b) => a.x - b.x);
-        const earliestDate = data[0].x;
-        const latestDate = data[data.length - 1].x;
-        const lowestValue = data.reduce((a, b) => a.y < b.y ? a : b).y;
-        const highestValue = data.reduce((a, b) => a.y > b.y ? a : b).y;
-        console.log(earliestDate, latestDate, lowestValue, highestValue);
 
-        const xScale = w / (data[data.length - 1].x - data[0].x);
-        //const yScale = 100 / (data[data.length - 1].y - data[0].y);
-        const yScale = 100 / (highestValue - lowestValue);
-        for (let i = 0; i < data.length; i++) {
+    #triggerUpdate() {
+        console.log('triggerUpdate', this.#dirty, this.#timeout);
+        if (this.#timeout) return; // already in progress
+        this.#timeout = setTimeout(() => {
+            console.log('timeout', this.refreshDelay);
+            this.#timeout = null;
+            this.update();
+        }, this.refreshDelay);
+        console.log(this.refreshDelay);
+    }
+    #sortData() {
+        this.rawData.sort((a, b) => a.x - b.x);
+    }
+
+    /**
+     * Force plot refresh
+     * @deprecated Normally called automatically
+     */
+    update() {
+        console.log('update', this.#dirty, this.rawData, this.#timeout);
+        this.svg.innerHTML = ""; // clear SVG
+        this.#dirty = false;
+        if (this.#timeout) {
+            clearTimeout(this.#timeout);
+            this.#timeout = null;
+        }
+        const [w,h] = [this.w, this.h];
+
+        const d = this.rawData;
+        if (!d.length) return;
+
+        d.sort((a, b) => a.x.valueOf() - b.x.valueOf());
+        const lowestValue = d.reduce((a, b) => a.y < b.y ? a : b).y;
+        const highestValue = d.reduce((a, b) => a.y > b.y ? a : b).y;
+
+        /** @type {number} */
+        const xLen = (d[d.length - 1].x.valueOf() - d[0].x.valueOf());
+        if (!xLen) return;
+
+        const xScale = (w-10) / xLen;
+        const yScale = h / (highestValue - lowestValue);
+        for (let i = 0; i < d.length; i++) {
             const rect = document.createElementNS(ns, "rect");
-            rect.setAttribute("x", Math.min(xScale * (data[i].x - data[0].x), w));
-            //rect.setAttribute("y", 100 - yScale * (data[i].y - data[0].y));
-            rect.setAttribute("y", 100 - yScale * (data[i].y - lowestValue));
-            rect.setAttribute("width", i < data.length - 1 ? xScale * (data[i + 1].x - data[i].x) : 0);
-            //rect.setAttribute("height", yScale * data[i].y);
-            rect.setAttribute("height", yScale * data[i].y);
-            rect.setAttribute("fill", "black");
+            rect.setAttribute("x", `${xScale * (d[i].x.valueOf() - d[0].x.valueOf())}`);
+            const wh = yScale * (d[i].y-lowestValue);
+            rect.setAttribute("y",`${100-wh}`);
+            rect.setAttribute("width",
+                (
+                    i < d.length-1
+                    ? xScale * (d[i + 1].x.valueOf() - d[i].x.valueOf())
+                    : 10
+                ).toString()
+            );
+            rect.setAttribute("height", `${wh}`);
+            rect.setAttribute("fill", i < d.length-1 ? "black" : "blue");
             rect.addEventListener("mouseover", () => {
                 const textVal = document.createElementNS(ns, "text");
                 const s = textVal.setAttribute.bind(textVal);
-                s('x',15);
-                s('y',15);
-                s('textLength', "100%");
+                s('x',0);
+                s('y',h*.15);
                 s('fill', "white");
                 s('stroke', "red");
                 s('stroke-width', 1);
                 s('font-family', "monospace");
-                textVal.textContent = `${data[i].x.toISOString()}, ${Math.round(data[i].y)}`;
+                textVal.textContent = `${d[i].x.toISOString()} ${Math.round(d[i].y)}`;
                 // create a clone of the text element
-                const textValClone = textVal.cloneNode(true);
+                /** @type {SVGTextElement} */
+                const textValClone = /** @type {SVGTextElement} */ (textVal.cloneNode(true));
                 textValClone.setAttribute("stroke", "white");
                 textValClone.setAttribute("stroke-width", "3");
                 this.svg.appendChild(textValClone);
@@ -124,38 +173,44 @@ export default class MTPlot {
         }
     }
 
-    addData(data) {
-        this.proxy.push(data);
+    /**
+     * Replace the newest (sorts, then replaces the first) data point
+     * @param {object} param0 
+     * @param {Date} param0.x
+     * @param {number} param0.y
+     */
+    replaceOldest({x, y}) {
+        if (this.#dirty) {
+            this.#sortData();
+        } else {
+            this.#dirty = true;
+        }
+        this.rawData[0] = {x, y};
+        this.#triggerUpdate();
     }
-    clearData() {
-        this.proxy = new Proxy(this.data, {
-            set: (target, property, value) => {
-                target[property] = value;
-                this.update();
-                return true;
-            }
-        });
+
+    /**
+     * Replace the newest (sorts, then replaces the last) data point
+     * @param {object} param0 
+     * @param {Date} param0.x
+     * @param {number} param0.y
+     */
+    replaceNewest({x, y}) {
+        if (this.#dirty) {
+            this.#sortData();
+        } else {
+            this.#dirty = true;
+        }
+        this.rawData[this.rawData.length-1] = {x, y};
+        this.#triggerUpdate();
     }
-    removeData(index) {
-        this.proxy.splice(index, 1);
-    }
-    getData() {
-        return this.proxy;
-    }
-    setData(data) {
-        this.proxy = new Proxy(data, {
-            set: (target, property, value) => {
-                target[property] = value;
-                this.update();
-                return true;
-            }
-        });
+    /**
+     * Reset data and clear the plot
+     * @returns {void}
+     */
+    reset() {
+        this.rawData.length = 0;
+        this.#dirty = true;
+        this.update();
     }
 }
-
-//window.MTPlot = MTPlot;
-
-/*
-
-mtplot = new MTPlot("mtplot", [{ x: new Date(2020, 0, 1), y: 1 }, { x: new Date(2020, 0, 2), y: 2 }]);
-*/
